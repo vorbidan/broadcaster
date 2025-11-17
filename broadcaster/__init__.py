693ed328dd88542b5d2d8a042c56883413407c25
vorbid6611E <vitaliy_orbidan@comcast.com>
2025-11-17 14:39:09 -0700

    Bumped up vrsion to 0.3.3

--- broadcaster/__init__.py
+++ broadcaster/__init__.py
@@ -1,5 +1,5 @@
 from ._base import Broadcast, Event
 from .backends.base import BroadcastBackend
 
-__version__ = "0.3.2"
+__version__ = "0.3.3"
 __all__ = ["Broadcast", "Event", "BroadcastBackend"]
